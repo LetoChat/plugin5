be8f67a8baad3380a8cc98ee66e71ad935f50b7a
Mihai Zinculescu <mihai.zinculescu@gmail.com>
2021-04-13 01:39:29 +0300

    added Tests

--- src/Widget.php
+++ src/Widget.php
@@ -1,15 +1,24 @@
 <?php
 
     namespace LetoChat;
-
+	
+	use Exception;
+	
     class Widget {
 
         protected $channelId;
         protected $channelSecret;
 
-        private $info = [];
+        private $info 	= [];
+		private $custom = [];
         private $events = [];
 
+		private $availableInfos = [
+			'id', 'name', 'avatar', 
+			'email', 'phone',
+			'company_name', 'company_position', 
+		];
+		
         public function __construct( $channelId, $channelSecret = null ){
 
             $this->channelId = $channelId;
@@ -19,13 +28,44 @@
 
         public function info( $name, $value ){
 
+			// checks
+			if( ! is_string($name) )
+				throw new Exception('Invalid value type for provided info. Info name must be string type.');
+			
+			if( in_array($name, ['custom']) )
+				throw new Exception('You can not add `custom` info. If you want to add a custom info use instead Widget::custom');
+			
+			if( in_array($name, ['event', 'events']) )
+				throw new Exception('You can not add `event` info. If you want to add an event use instead Widget::event');
+			
+			$name = strtolower($name);
+			
+			if( ! in_array($name, $this->availableInfos) )
+				throw new Exception('Invalid info `'.$name.'`. Info must one of: '.implode(', ', $this->availableInfos).'.');
+		
+			if( ! is_string($value) && ! is_numeric($value) && ! is_bool($value) )
+				throw new Exception('Invalid value type for `'.$name.'`. Value must be string, numeric or boolean.');
+		
+			// done
+			$this->info[ $name ] = $value;
 
             return $this;
 
         }
 
-        public function infos(){
+        public function infos( $infos ){
 
+			if( ! is_array($infos) )
+				throw new Exception('If you want to add infos, you have to provide as array');
+		
+			foreach($infos as $n => $v){
+				try{
+					$this->info($n, $v);
+				} catch (Exception $e ){
+					throw $e;
+				}
+			}
+		
             return $this;
 
         }
@@ -42,12 +82,28 @@
 
         }
 
+		
+		public function custom(){
+
+
+            return $this;
+
+        }
+		
+		public function getCustom(){
+
+            return $this->custom;
+
+        }
+		
+		
         public function event(){
 
 
             return $this;
 
         }
+		
 
         public function getEvent( $name ){
 
@@ -60,11 +116,15 @@
             return $this->events;
 
         }
+		
+		
 
         public function build( $includeScriptTag = true ){
 
-            $script = '';
+            $script = 'console.log("LetoChat is Loaded");';
 
+            if( $includeScriptTag )
+                $script = '<script>'.$script.'</script>';
 
             return $script;
 
